da3adc842bfa97f591371e11f8b91dab336e9b61
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-23 11:17:46 +0200

    0.3.0 - 2023-08-23
    * Added serialization test support

---
+++ README.md
@@ -0,0 +1,8 @@
+# fj-test-helper8
+
+**Basic helper classes for testing on java 8+**
+
+*Changelog*
+
+## 0.3.0 - 2023-08-23
+* Added serialization test support
--- fj-test-helper8/src/main/java/test/org/fugerit/java/BasicTest.java
+++ fj-test-helper8/src/main/java/test/org/fugerit/java/BasicTest.java
@@ -2,6 +2,12 @@ package test.org.fugerit.java;
 
 import static org.junit.Assert.fail;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +19,33 @@ public class BasicTest {
 		String message = "Error : "+e;
 		logger.error( "Error "+e, e );
 		fail( message );
+	}
+	
+	public Object fullSerializationTest( Object value ) throws IOException {
+		byte[] data = this.serializeTest(value);
+		return this.deserializeTest(data);
+	}
+	
+	public Object deserializeTest( byte[] data ) throws IOException {
+		Object res = null;
+		try ( ObjectInputStream ois = new ObjectInputStream( new ByteArrayInputStream( data ) ) ) {
+			res = ois.readObject();
+			logger.info( "deserializeTest, read object : {}", res );
+		} catch (ClassNotFoundException e) {
+			throw new IOException( e.toString(), e );
+		}
+		return res;
+	}
+	
+	public byte[] serializeTest( Object value ) throws IOException {
+		byte[] res = null;
+		try ( ByteArrayOutputStream os = new ByteArrayOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream( os ) ) {
+			oos.writeObject( value );
+			res = os.toByteArray();
+			logger.info( "serializeTest, total byte written : {}", res.length );
+		}
+		return res;
 	}
 	
 }

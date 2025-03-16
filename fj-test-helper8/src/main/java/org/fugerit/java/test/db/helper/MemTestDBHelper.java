c5a0d43704ebbe7f3a08d867b0c87b361ec38902
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-03-16 21:31:08 +0100

    Fix sonar issues

--- fj-test-helper8/src/main/java/org/fugerit/java/test/db/helper/MemTestDBHelper.java
+++ fj-test-helper8/src/main/java/org/fugerit/java/test/db/helper/MemTestDBHelper.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
-import org.fugerit.java.core.cfg.ConfigRuntimeException;
 import org.fugerit.java.core.db.connect.ConnectionFactory;
 import org.fugerit.java.core.db.dao.DAOException;
 import org.fugerit.java.core.db.helpers.SQLScriptReader;
